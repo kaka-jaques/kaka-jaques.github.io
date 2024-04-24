74b25afd9e08875a71f6edf216070914cab9cf0d
Kalil J. Fakhouri <kjfakhouri@gmail.com>
2024-04-24 00:51:02 -0300

    updated projects

--- js/main.js
+++ js/main.js
@@ -36,27 +36,54 @@ var swiper1 = new Swiper(".projectSwiper", {
 var projectTitle = 
 [
   {
-    title: "Project X",
-    description: "Descrição do Projeto X"
+    title: "Sistema TMS",
+    description: "Projeto de apresentação desenvolvido para o Curso Entra21 da Proway. O sistema tem as funcionalidades de login com base na função, cotação e gerenciamento de fretes com localizações reais (Stacks: Java, SQL, Angular)",
+    link: "https://github.com/kaka-jaques/Project_TMS_Fullstack_Entra21"
   },
   {
-    title: "Project Y",
-    description: "Descrição do Projeto Y"
+    title: "Money Conversor App",
+    description: "Aplicativo Android de conversão de moedas (US$ x R$) com atualização da moeda em tempo real. (Stacks: Java - Android)",
+    link: "https://github.com/kaka-jaques/moneyconversor_app_udemy_course"
   },
   {
-    title: "Project Z",
-    description: "Descrição do Projeto Z"
+    title: "Projeto SCCH",
+    description: "(Em Desenvolvimento)Projeto extensionista do Curso de Análise e Desenvolvimento de Sistemas da UNINTER. O sistema tem como base informar as melhoras peças com custo x benefício para o cliente dependendo de sua necessidade. (Stacks: Java, SQL, Angular)",
+    link: "https://github.com/kaka-jaques/UNINTER_SCCH_Project"
   },
   {
-    title: "Project A",
-    description: "Descrição do Projeto A"
+    title: "Automatizer Backup",
+    description: "Automação para realizar backup em rede ou localmente dos arquivos no PC desejado. (Stacks: Batchfile)",
+    link: "https://github.com/kaka-jaques/automatizer-backup"
+  },
+  {
+    title: "Nadalini Website",
+    description: "Website desenvolvido para a empresa Nadalini Representações com API própria E aplicativo para gerenciar lançamento de promoções no site. (Stacks: Java, SQL, Angular, HTML, CSS, JavaScript, DevOps)",
+    link: "https://nadalini.com.br"
+  },
+  {
+    title: "GM Autocenter Website",
+    description: "Website desenvolvido para a empresa GM Autocenter apenas para fins informativos. (Stacks: HTML, CSS, JavaScript, DevOps)",
+    link: "https://gmautocenter.com.br"
+  },
+  {
+    title: "Prediletus Website",
+    description: "(Em negociação) Website informativo em negociação para a empresa Prediletus. (Stacks: HTML, CSS, JavaScript, DevOps)",
+    link: "https://prediletus.com.br"
   }
 ]
 
-document.getElementById("project-title").innerHTML = "<h1 class='title'>" + projectTitle[0].title + "</h1><p class='description'>" + projectTitle[0].description + "</p>"
+document.getElementById('project-title').addEventListener('click', () => {
+  window.open(projectTitle[swiper1.realIndex].link, '_blank')
+})
+
+document.getElementById("project-title").innerHTML = "<div class='title'><h1>" + projectTitle[0].title + "</h1></div><div class='description'><p>" + projectTitle[0].description + "</p></div>"
+
+let transitioning = false;
 
 setInterval(function(){
-  if(swiper1.animating == true){
+
+  if(swiper1.animating == true && transitioning == false){
+    transitioning = true;
     gsap.to(".project-title", {
       opacity:0,
       duration: 0.5,
@@ -66,12 +93,15 @@ setInterval(function(){
         gsap.to(".project-title", {
           opacity:1,
           duration: 0.5,
-          y: 0
+          y: 0,
+          onComplete: () => {
+            transitioning = false
+          }
         })
       }
     })
   }
-}, 500)
+}, 200)
 
 // var bar = new ProgressBar.Circle(container, {
 //   strokeWidth: 5,
