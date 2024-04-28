841cc406bd4f46bacd80553dedba749248a59d80
Kalil J. Fakhouri <kjfakhouri@gmail.com>
2024-04-27 22:05:01 -0300

    update header & loader entry

--- js/main.js
+++ js/main.js
@@ -3,8 +3,34 @@ if (navigator.language === 'en-US' && !window.location.href.includes('us')) {
   window.location.href += '/us'
 }
 
+window.addEventListener('load', function () {
+  const AllImages = document.querySelectorAll('img');
+
+  function verificaLoadImg() {
+    let imagensCarregadas = true;
+
+    AllImages.forEach((img) => {
+      if (!img.complete && !img.naturalHeight) {
+        imagensCarregadas = false;
+        return
+      }
+    })
+
+    if (imagensCarregadas) {
+      enableOverflow();
+    }
+
+  }
+
+  AllImages.forEach((img) => {
+    img.addEventListener('load', verificaLoadImg)
+  })
+
+  verificaLoadImg();
+})
+
 //INICIANDO O SITE COM SCROLL TRAVADO
-document.body.style.overflow = 'auto';
+document.body.style.overflow = 'hidden';
 
 //PROGRESS BAR DO CURSO UNINTER
 var hoje = Date.now() - Date.UTC(2022, 12)
@@ -12,65 +38,65 @@ var conclusaoCursoUninter = (hoje * 100) / (Date.UTC(2025, 6) - Date.UTC(2022, 1
 
 var swiper1 = new Swiper(".projectSwiper", {
   effect: "coverflow",
-      grabCursor: true,
-      loop: true,
-      centeredSlides: true,
-      slidesPerView: "3",
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-      },
-      autoplay: {
-        delay: 6500
-      },
-      speed: 900,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    }
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: "3",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  autoplay: {
+    delay: 6500
+  },
+  speed: 900,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  }
 })
 
-var projectTitle = 
-[
-  {
-    title: "Sistema TMS",
-    description: "Projeto de apresentação desenvolvido para o Curso Entra21 da Proway. O sistema tem as funcionalidades de login com base na função, cotação e gerenciamento de fretes com localizações reais (Stacks: Java, SQL, Angular)",
-    link: "https://github.com/kaka-jaques/Project_TMS_Fullstack_Entra21"
-  },
-  {
-    title: "Money Conversor App",
-    description: "Aplicativo Android de conversão de moedas (US$ x R$) com atualização da moeda em tempo real. (Stacks: Java - Android)",
-    link: "https://github.com/kaka-jaques/moneyconversor_app_udemy_course"
-  },
-  {
-    title: "Projeto SCCH",
-    description: "(Em Desenvolvimento)Projeto extensionista do Curso de Análise e Desenvolvimento de Sistemas da UNINTER. O sistema tem como base informar as melhoras peças com custo x benefício para o cliente dependendo de sua necessidade. (Stacks: Java, SQL, Angular)",
-    link: "https://github.com/kaka-jaques/UNINTER_SCCH_Project"
-  },
-  {
-    title: "Automatizer Backup",
-    description: "Automação para realizar backup em rede ou localmente dos arquivos no PC desejado. (Stacks: Batchfile)",
-    link: "https://github.com/kaka-jaques/automatizer-backup"
-  },
-  {
-    title: "Nadalini Website",
-    description: "Website desenvolvido para a empresa Nadalini Representações com API própria E aplicativo para gerenciar lançamento de promoções no site. (Stacks: Java, SQL, Angular, HTML, CSS, JavaScript, DevOps)",
-    link: "https://nadalini.com.br"
-  },
-  {
-    title: "GM Autocenter Website",
-    description: "Website desenvolvido para a empresa GM Autocenter apenas para fins informativos. (Stacks: HTML, CSS, JavaScript, DevOps)",
-    link: "https://gmautocenter.com.br"
-  },
-  {
-    title: "Prediletus Website",
-    description: "(Em negociação) Website informativo em negociação para a empresa Prediletus. (Stacks: HTML, CSS, JavaScript, DevOps)",
-    link: "https://prediletus.com.br"
-  }
-]
+var projectTitle =
+  [
+    {
+      title: "Sistema TMS",
+      description: "Projeto de apresentação desenvolvido para o Curso Entra21 da Proway. O sistema tem as funcionalidades de login com base na função, cotação e gerenciamento de fretes com localizações reais (Stacks: Java, SQL, Angular)",
+      link: "https://github.com/kaka-jaques/Project_TMS_Fullstack_Entra21"
+    },
+    {
+      title: "Money Conversor App",
+      description: "Aplicativo Android de conversão de moedas (US$ x R$) com atualização da moeda em tempo real. (Stacks: Java - Android)",
+      link: "https://github.com/kaka-jaques/moneyconversor_app_udemy_course"
+    },
+    {
+      title: "Projeto SCCH",
+      description: "(Em Desenvolvimento)Projeto extensionista do Curso de Análise e Desenvolvimento de Sistemas da UNINTER. O sistema tem como base informar as melhoras peças com custo x benefício para o cliente dependendo de sua necessidade. (Stacks: Java, SQL, Angular)",
+      link: "https://github.com/kaka-jaques/UNINTER_SCCH_Project"
+    },
+    {
+      title: "Automatizer Backup",
+      description: "Automação para realizar backup em rede ou localmente dos arquivos no PC desejado. (Stacks: Batchfile)",
+      link: "https://github.com/kaka-jaques/automatizer-backup"
+    },
+    {
+      title: "Nadalini Website",
+      description: "Website desenvolvido para a empresa Nadalini Representações com API própria E aplicativo para gerenciar lançamento de promoções no site. (Stacks: Java, SQL, Angular, HTML, CSS, JavaScript, DevOps)",
+      link: "https://nadalini.com.br"
+    },
+    {
+      title: "GM Autocenter Website",
+      description: "Website desenvolvido para a empresa GM Autocenter apenas para fins informativos. (Stacks: HTML, CSS, JavaScript, DevOps)",
+      link: "https://gmautocenter.com.br"
+    },
+    {
+      title: "Prediletus Website",
+      description: "(Em negociação) Website informativo em negociação para a empresa Prediletus. (Stacks: HTML, CSS, JavaScript, DevOps)",
+      link: "https://prediletus.com.br"
+    }
+  ]
 
 document.getElementById('project-title').addEventListener('click', () => {
   window.open(projectTitle[swiper1.realIndex].link, '_blank')
@@ -80,18 +106,18 @@ document.getElementById("project-title").innerHTML = "<div class='title'><h1>" +
 
 let transitioning = false;
 
-setInterval(function(){
+setInterval(function () {
 
-  if(swiper1.animating == true && transitioning == false){
+  if (swiper1.animating == true && transitioning == false) {
     transitioning = true;
     gsap.to(".project-title", {
-      opacity:0,
+      opacity: 0,
       duration: 0.5,
       y: -100,
       onComplete: () => {
         document.getElementById("project-title").innerHTML = "<h1 class='title'>" + projectTitle[swiper1.realIndex].title + "</h1><p class='description'>" + projectTitle[swiper1.realIndex].description + "</p>"
         gsap.to(".project-title", {
-          opacity:1,
+          opacity: 1,
           duration: 0.5,
           y: 0,
           onComplete: () => {
@@ -109,8 +135,8 @@ const divTitle = document.querySelector('.project-title');
 function atualizaPosicao() {
   mainPos = divSwiper.getBoundingClientRect();
 
-  divTitle.style.left = (mainPos.left*2.05)+window.scrollX + 'px';
-  divTitle.style.top = mainPos.top+window.scrollY+150 + 'px';
+  divTitle.style.left = (mainPos.left * 2.05) + window.scrollX + 'px';
+  divTitle.style.top = mainPos.top + window.scrollY + 150 + 'px';
 
 }
 
